31b7a82052e5cc02aa59a643c678bd875e5ebff9
PMaini532 <prathammaini5@gmail.com>
2024-03-06 21:15:46 +0530

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,13 +1,22 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from '../api/routes/user.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log('Mongodb is connected');
+}).catch((err)=>{
+    console.log(err);
 });
 const app = express();
 
 app.listen(3000,()=>{
     console.log('Server is running');
 })
+
+// app.get('/test',(req,res)=>{
+//     res.json({message:'API IS WORKING'});
+// });
+
+app.use('/api/user',userRoutes);
