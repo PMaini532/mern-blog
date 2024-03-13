1c0daee0268b1ac7924c450410b5c0804061c4d2
PMaini532 <prathammaini5@gmail.com>
2024-03-13 22:37:44 +0530

    sign out functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -8,7 +8,8 @@ import {
     updateSuccess,
     updateFailure,
     deleteUserStart,
-    deleteUserSuccess
+    deleteUserSuccess,
+    signoutSuccess,
 } from '../redux/user/userSlice.js';
 import { useDispatch } from 'react-redux';
 import 'react-circular-progressbar/dist/styles.css';
@@ -126,6 +127,21 @@ export default function DashProfile() {
     }
   };
 
+  const handleSignout = async () =>{
+    try{
+        const res = await fetch('api/user/signout',{
+            method: 'POST',
+        });
+        const data = await res.json();
+        if(!res.ok){
+            console.log(data.message);
+        }else{
+            dispatch(signoutSuccess());
+        }
+    }catch(error){
+        console.log(error.message);
+    }
+  };
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
         <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
@@ -180,7 +196,7 @@ export default function DashProfile() {
         </form>
         <div className="text-red-500 flex justify-between mt-5">
             <span onClick={()=>setShowModal(true)} className='cursor-pointer'>Delete Account</span>
-            <span className='cursor-pointer'>Sign Out</span>
+            <span onClick={handleSignout} className='cursor-pointer'>Sign Out</span>
         </div>
         {updateUserSuccess && (
         <Alert color='success' className='mt-5'>
