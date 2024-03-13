e554d0945f52602905b396c62c8ac92e95433f2c
PMaini532 <prathammaini5@gmail.com>
2024-03-13 14:36:31 +0530

    user update functionality

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -23,6 +23,19 @@ const userSlice = createSlice({
       state.loading = false;
       state.error = action.payload;
     },
+    updateStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    updateSuccess: (state, action) => {
+      state.currentuser = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    updateFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
   },
 });
 
@@ -30,7 +43,9 @@ export const {
   signInStart,
   signInSuccess,
   signInFailure,
-
+  updateStart,
+  updateSuccess,
+  updateFailure,
 } = userSlice.actions;
 
 export default userSlice.reducer;
