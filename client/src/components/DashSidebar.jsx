e4ee124b526d3071e577609d5f87e5b9bb7c49ca
PMaini532 <prathammaini5@gmail.com>
2024-03-14 23:24:57 +0530

    posts in dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,14 +1,16 @@
 import {Sidebar} from 'flowbite-react'
-import { HiUser,HiArrowSmRight } from 'react-icons/hi';
+import { HiUser,HiArrowSmRight,HiDocumentText } from 'react-icons/hi';
 import {useEffect,useState} from 'react'
 import {useLocation,Link} from 'react-router-dom';
 import { signoutSuccess } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 export default function DashProfile() {
     const location = useLocation();
     const [tab,setTab] = useState('');
     const dispatch = useDispatch();
+    const { currentuser} = useSelector((state) => state.user);
     useEffect(()=>{
         const urlParams = new URLSearchParams(location.search);
         const tabFromUrl = urlParams.get('tab');
@@ -35,12 +37,24 @@ export default function DashProfile() {
   return (
     <Sidebar className='w-full md:w-56'>
       <Sidebar.Items>
-        <Sidebar.ItemGroup>
+        <Sidebar.ItemGroup className='flex flex-col gap-1'>
             <Link to='/dashboard?tab=profile' >
-                <Sidebar.Item active={tab==='profile'} icon={HiUser} label={'User'} labelColor='dark' as='div'>
+                <Sidebar.Item active={tab==='profile'} icon={HiUser} label={ currentuser.isAdmin ? 'Admin':'User'} labelColor='dark' as='div'>
                     Profile
                 </Sidebar.Item>
             </Link>
+            {currentuser.isAdmin &&(
+                <Link to='/dashboard?tab=posts'>
+                <Sidebar.Item
+                    active={tab === 'posts'}
+                    icon={HiDocumentText} 
+                    as='div'
+                >
+                    Posts
+                </Sidebar.Item>
+            </Link>
+            )}
+            
             <Sidebar.Item  icon={HiArrowSmRight} className='cursor-pointer' onClick={handleSignout}>
                 Sign Out
             </Sidebar.Item>
